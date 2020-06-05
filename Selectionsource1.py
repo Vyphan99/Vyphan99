8472788060f1ceb38b5353656384e798f16b41eb
Vyphan99 <Vyphan99@users.noreply.github.com>
2020-06-04 23:21:29 -0700

    Selection source 1

--- Selectionsource1.py
+++ Selectionsource1.py
@@ -1,25 +1,25 @@
 
-array = [13, 4, 9, 5, 3, 16, 12]
+array = [13, 4, 9, 5, 3, 16, 12] #<-- Create an array of number 
 
-def seclectionSort(array):
+def selectionSort(array): #<-- Define a function to sort array 
 
-    n = len(array)
+    n = len(array) #<-- Define "n" as the length of the array 
 
     for i in range(n): #<--Whatever the length of the array is how many times you are going to run the for loop 
 
         #Initally assume the first element of the unsorted part as the minimun
 
-        minimum = i
+        minimum = i #<-- Define minumun as i 
 
         for j in range(i+1, n):
             #Move the smallest value up first
             if(array[j] < array[minimum]): #<--Comparision Operator
             
         #Swap the minimum element with the first element of the unsorted part 
-        temp = array[i]
-        array[i] = array[minimum]
-        array[minimum] = temp
+        temp = array[i] #<--Temporary value: a place holder to substitute value later on 
+        array[i] = array[minimum] #<-- Create user input array elements to be equal to the minimum element
+        array[minimum] = temp #<-- Create a temporary placement for the minimum elements 
 
-    return array
+    return array #<-- Calling the array 
 
-print(selectionSort(array))
+print(selectionSort(array)) #<-- Print selection array 
