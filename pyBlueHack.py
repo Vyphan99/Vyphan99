fd0af512f2bf002e7a2ec4bb3794dfbc7d16576c
Vyphan99 <Vyphan99@users.noreply.github.com>
2020-06-05 09:47:10 -0700

    New Python hack

--- pyBlueHack.py
+++ pyBlueHack.py
@@ -8,10 +8,11 @@ from bluetooth.ble import BeaconServices #<-- 3rd party module
 
 service = BeaconServices() #<--Create a Beacon service
 
-service.start_advertising("11111111-2222-3333-4444-555555555555, 1, 1, 1, 200")
+#start avertising with   |               UUIDs                 | Parameters 
+service.start_advertising("11111111-2222-3333-4444-555555555555, 1, 1, 1, 200") #<-- TCP,UDP
 
 time.sleep(15) #<--Stop the service at every 15 sec 
 
-service.stop_advertising()
+service.stop_advertising() #<-- Create a stop service to stop every 15 sec 
 
-print("Done.")
+print("Done.")#<--Print function: print the "Done." message on the screen 
